d8ae28a603a8a26627fdb35ce86defca94c22f82
jorixama <jorixama16@gmail.com>
2023-02-24 09:12:02 -0800

    matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,8 +7,8 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
     mat = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
-    for i in range(len(mat1)):  #iterate over rows of mat1
-        for j in range(len(mat2[0])):  #iterate over cols of mat2
-            for k in range(len(mat2)):  #iterate over comon dimension
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
                 mat[i][j] += mat1[i][k] * mat2[k][j]
     return mat
