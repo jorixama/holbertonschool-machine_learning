3c595f352ead2980b5ffca031ab3a948eea1e3ed
jorixama <jorixama16@gmail.com>
2023-02-24 04:42:38 -0800

    the transpose of a 2D matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
+"""a function that returns the transpose of a 2D matrix"""
+
+
 def matrix_transpose(matrix):
-    result=[]
-    for i in range (len(matrix[0])):
+"""returns the transpose of a 2D matrix"""
+    result = []
+    for i in range(len(matrix[0])):
         row = []
         for j in range(len(matrix)):
             row.append(matrix[j][i])
         result.append(row)
     return result
-mat1 = [[1, 2], [3, 4]]
-print(mat1)
-print(matrix_transpose(mat1))
-mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print(mat2)
-print(matrix_transpose(mat2))
