9ecc5657256045c72dbc2967ef44552baaf450be
jorixama <jorixama16@gmail.com>
2023-02-24 14:05:52 -0800

    concatenate two matrices

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """that concatenates two matrices along a specific axis"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices along a specific axis"""
     if axis == 0:
@@ -8,7 +10,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         result = mat1 + mat2
     elif axis == 1:
         if len(mat1) != len(mat2):
-            return None   
+            return None
         result = [mat1[i] + mat2[i] for i in range(len(mat1))]
     else:
         return None
