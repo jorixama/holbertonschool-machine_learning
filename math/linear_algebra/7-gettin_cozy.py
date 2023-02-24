069c3a1e3d27f635a2c74ef8e04aee9b28abbefe
jorixama <jorixama16@gmail.com>
2023-02-24 11:51:25 -0800

    concatenate two matrices

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,7 +10,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         result = mat1 + mat2
     elif axis == 1:
         if len(mat1) != len(mat2):
-               return None   
+            return None   
         result = [mat1[i] + mat2[i] for i in range(len(mat1))]
     else:
         return None
