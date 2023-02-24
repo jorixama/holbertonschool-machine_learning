438ac1b879ea9b2f19990c36227b4734286928c6
jorixama <jorixama16@gmail.com>
2023-02-24 05:14:39 -0800

    add two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
+"""a function that adds two matrices element-wise"""
+
+
 def add_matrices2D(mat1, mat2):
+    """adds two matrices element-wise"""
     mat = []
     if len(mat1) != len(mat2[0]) or len(mat1) != len(mat2):
         return None
     for i in range(len(mat1)):
-        row =[]
+        row = []
         for j in range(len(mat1[0])):
             row.append((mat1[i][j]) + (mat2[i][j]))
         mat.append(row)
     return mat
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
