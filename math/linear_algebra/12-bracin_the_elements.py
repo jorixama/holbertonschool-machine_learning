e7306873dd476eb35e6c544039784fb1f9db8c19
jorixama <jorixama16@gmail.com>
2023-02-24 14:47:05 -0800

    element-wise

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""bracing"""
+
+import numpy as np
+"""transpose numpy"""
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+mat1 = mat1.T
+mat2 = mat2.T
+print(mat1)
+print(mat2)
+
+add, sub, mul, div = np_elementwise(mat1, mat2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+add, sub, mul, div = np_elementwise(mat1, 2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
