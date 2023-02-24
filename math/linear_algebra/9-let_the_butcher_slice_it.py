8109b54e1e085f8eea65690df346572ee9cd2b51
jorixama <jorixama16@gmail.com>
2023-02-24 12:54:57 -0800

    concatenate two matrices

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""matrix"""
 
+import numpy as np
+mat1 =  matrix[[1,2]]
+mat2 = matrix[:, [2, 3]]
+mat3 = matrix [1:4, 3:6]
+print("The middle two rows of the matrix are:\n{}".format(mat1))
+print("The middle two columns of the matrix are:\n{}".format(mat2))
+print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
