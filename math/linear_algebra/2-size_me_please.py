a759e5489e99e7b14c5e80f6fe367836737023f5
jorixama <jorixama16@gmail.com>
2023-02-24 03:43:46 -0800

    concatenate two arrays

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,7 +2,6 @@
 """a function that returns the shape of a matrix"""
 
 def matrix_shape(matrix):
-"""returns the shape of a matrix"""
     shape=[]
         while isinstance(matrix,list):
             shape.append(len(matrix))
