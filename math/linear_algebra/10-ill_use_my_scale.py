805a337e6d56842a22b490319ea74bef916949ab
jorixama <jorixama16@gmail.com>
2023-02-24 13:29:19 -0800

    shape numpy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""numpy shape"""
+
+import numpy as np
+def np_shape(matrix):
+    return np.ndarray(mat1, mat2, mat3)
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+print(np_shape(mat1))
+print(np_shape(mat2))
+print(np_shape(mat3))
