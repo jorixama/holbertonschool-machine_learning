b8cba5c8896b50862e40d1d5b43f24e71a9667dc
jorixama <jorixama16@gmail.com>
2023-02-23 17:04:09 -0800

    two matrices element-wise

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -7,9 +7,3 @@ def add_arrays(arr1, arr2):
         for i in range(len(arr1)):
             result.append(arr1[i] + arr2[i])
         return result
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
