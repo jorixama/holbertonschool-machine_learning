ad6535167105759215ebb85677fb377852f3f38a
jorixama <jorixama16@gmail.com>
2023-02-24 14:33:25 -0800

    concate two matrices

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -9,7 +9,3 @@ def np_cat(mat1, mat2, axis = 1):
     return np.concatenate((mat1, mat2), axis=1)
 def np_cat(mat1, mat3, axis = 1):
     return np.concatenate((mat1, mat3), axis=1)
-return result
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-mat3 = np.array([[7], [8]])
